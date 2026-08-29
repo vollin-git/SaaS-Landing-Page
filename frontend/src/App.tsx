49eb3af3521f9b29304218e9c5076723e277de1b
vollin-git <vollinferns@gmail.com>
2026-08-29 17:08:06 +0530

    code structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center">
+        Welcome back
+      </h1>
+    </div>
+      
+  )
+}
+
+export default App
